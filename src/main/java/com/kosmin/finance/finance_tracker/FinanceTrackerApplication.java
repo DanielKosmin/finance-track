b6f7ec9766b69ddc0f582302a8b2052cf61d4af3
Daniel Kosmin <kosmindaniel@gmail.com>
2024-10-12 15:02:34 -0400

    initial

---
+++ src/main/java/com/kosmin/finance/finance_tracker/FinanceTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.kosmin.finance.finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinanceTrackerApplication.class, args);
+  }
+}
